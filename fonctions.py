b0c4d5a4e9c30f38715cbda92e8592faf3431474
marvinchebany94 <87269698+marvinchebany94@users.noreply.github.com>
2021-08-26 16:47:36 +0200

    Add files via upload

--- fonctions.py
+++ fonctions.py
@@ -2,7 +2,9 @@
 Ce fichier va contenir toutes les fonctions du projet
 """
 import random
-def system_suisse(liste_joueur):
+from tinydb import TinyDB, where, Query
+from modele import db, players_table, liste_joueur, Matchs
+def creation_paires(liste_joueur):
     """
     la fonction va créer 2 listes, les 4 meilleurs joueurs et les 4 moins bons.
     Dans un second temps elle va associer le 1er joueur de la 1ère liste avec le 1er de la deuxième et
@@ -14,18 +16,87 @@ def system_suisse(liste_joueur):
     :param liste_joueur:
     :return:
     """
+    #on crée une liste contenant seulement le classement des joueurs
+    liste_classement_joueur = []
+    for item in players_table:
+        liste_classement_joueur.append(item['classement'])
 
-    """
-    On va 
-    on va créer les variables des 2 listes
-    """
-    liste_meilleurs_joueurs = liste_joueur
+    #on trie la liste en la mettant par ordre croissant
+    liste_joueur_classement_croissant = sorted(liste_classement_joueur)
 
-    """
-    pour trier la liste des joueurs on va utiliser sorted(liste)
-    """
+    #on reprend la liste classée par ordre croissant, puis on associe le rang des personnes à leur prénoms
+    liste_classement_joueur_avec_nom = []
+
+    for classement in liste_joueur_classement_croissant:
+
+        #on va chercher la personne qui a le classement dans la base de donnée, player_infos va ressortir la
+        #base de donnée qui correspond à celle-ci
+        player_infos = players_table.search(where('classement') == classement)
+        #on crée une boucle for pour prendre seulement le prénom et l'ajouter à la liste
+        for infos in player_infos:
+            print(infos['prenom'])
+            prenom_nom = infos['prenom'] + " " + infos['nom']
+            liste_classement_joueur_avec_nom.append(prenom_nom)
+
+    #on crée les 2 groupes
+    premier_groupe = liste_classement_joueur_avec_nom[0:4]
+    deuxieme_groupe = liste_classement_joueur_avec_nom[4:8]
+
+    #on crée les 4 paires
+    paire_1 = (premier_groupe[0], deuxieme_groupe[0])
+    paire_2 = (premier_groupe[1], deuxieme_groupe[1])
+    paire_3 = (premier_groupe[2], deuxieme_groupe[2])
+    paire_4 = (premier_groupe[3], deuxieme_groupe[3])
+
+    #on retourne la liste des 4 paires
+    return [paire_1, paire_2, paire_3, paire_4]
+
+#on crée une fonction qui prendra en paramétre la paire, le score et qui va retourner un tuple contenant
+#2 listes avec instance de joueur + le score
+def matchs(paires):
+    #id du match
+    id = 0
+    liste_id_et_tuples = []
+    for pair in paires:
+        id += 1
+        score_1 = random.randint(0, 1)
+        score_2 = random.randint(0, 1)
+        while score_2 == score_1:
+            score_2 = random.randint(0, 1)
+        tuple = ([pair[0], score_1], [pair[1], score_2])
+        match = Matchs(id, tuple)
+
+        liste_id_et_tuples.append([match.id, match.tuple])
+        #liste_tuple.append(match.tuple)
+
+    for id_tuple in liste_id_et_tuples:
+        id = id_tuple[0]
+        tuple = id_tuple[1]
+        match = Matchs(id, tuple)
+        try:
+            db = TinyDB('db.json')
+            matchs_table = db.table('matches')
+            match_serialized = {
+                'id':match.id,
+                'tuple':match.tuple
+            }
+            matchs_table.insert(match_serialized)
+
+        except:
+            print("une erreur a été dectecté lors de la création de la bdd")
+    matchs_table = matchs_table.all()
+    print(matchs_table)
 
 
-    print(sorted(liste_joueur))
-liste = [10,5,6,1,3,7]
-system_suisse(liste)
+
+
+
+    #pour chaque paires
+
+
+paires = creation_paires(liste_joueur)
+print(paires)
+matchs(paires)
+
+
+
--- modele.py
+++ modele.py
@@ -1,4 +1,4 @@
-
+from tinydb import TinyDB
 """
 Dans ce fichier on va créer toutes les classes pour le script
 """
@@ -12,11 +12,76 @@ class Joueur:
         self.classement = classement
 
 class Tournois:
-    def __init__(self, nom, lieu, date, nombre_de_tours, tournees):
+    def __init__(self, nom, lieu, date, nombre_de_tours, tournees, liste_des_joueurs, description):
         self.nom = nom
         self.lieu = lieu
         self.date = date
         self.nombre_de_tours = nombre_de_tours
         nombre_de_tours = 4
         self.tournees = tournees
+        self.liste_des_joueurs = liste_des_joueurs
+        Liste_des_joueurs = []
+        self.description = description
+
+class Matchs:
+    def __init__(self, id, tuple):
+        self.id = id
+        self.tuple = tuple
+
+
+marvin = Joueur("de cocq", "marvin", "20/07/98", "masculin", 1)
+marvin2 = Joueur("chebany", "rocket", "20/07/98", "masculin", 3)
+marvin3 = Joueur("dunoyer", "kahyss", "16/03/2019", "masculin", 10)
+loan = Joueur("dunoyer", "loan", "01/11/00", "féminin", 100)
+papa = Joueur("de cocq", "martial", "13/10/71", "masculin", 41)
+maman = Joueur("chebany", "angela", "16/03/2019", "féminin", 2)
+mamie = Joueur("chebany", "henriette", "09/06/50", "féminin", 34)
+papy = Joueur("chebany", "cheban", "12/04/44", "masculin", 68)
+
+
+
+liste_joueur = [marvin, marvin2, marvin3, loan, papa, maman, mamie, papy]
+tournois = Tournois('rocket league', 'val-de-marne', "28/08/21","4","none", liste_joueur, "Beau tournois")
+db = TinyDB('db.json')
+players_table = db.table('joueurs')
+tournois_table = db.table('tournois')
+players_table.truncate()	# clear the table first
+
+for player in liste_joueur:
+    serialized_player = {
+        'nom': player.nom,
+        'prenom': player.prenom,
+        'date de naissance': player.date_de_naissance,
+        'sexe': player.sexe,
+        'classement': player.classement
+
+    }
+    players_table.insert(serialized_player)
+serialized_player = players_table.all()
+print(serialized_player)
+
+liste_joueur = []
+for item in players_table:
+    liste_joueur.append(item)
+
+
+
+
+"""
+serialized_tournois = {
+    'nom': tournois.nom,
+    'lieu':tournois.lieu,
+    'date':tournois.date,
+    'nombre de tour':tournois.nombre_de_tours,
+    'tournees':tournois.tournees,
+    'liste de joueurs':liste_joueur,
+    'description':tournois.description
+}
+
+tournois_table.insert(serialized_tournois)
+serialized_tournois = tournois_table.all()
+
+print(serialized_tournois)
+
+"""
 
